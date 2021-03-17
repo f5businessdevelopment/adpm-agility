ec10c135aa741dc2592eac9e014704ed59264451
gregcoward <g.coward@f5.com>
2021-03-17 16:24:40 -0700

    update

--- terraform/deploy/main.tf
+++ terraform/deploy/main.tf
@@ -552,5 +552,11 @@ resource "azurerm_virtual_machine" "elkvm" {
       "chmod +x /home/elkuser/elkupdate.sh",
       "/home/elkuser/elkupdate.sh",
     ]
+    connection {
+      type     = "ssh"
+      user     = "elkuser"
+      password = var.upassword
+      host     = data.azurerm_public_ip.elk_public_ip.ip_address
+    }
   }
 }
