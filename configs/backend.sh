2674f44cb5c31b39f182c145126b29dc2ac2daad
gregcoward <g.coward@f5.com>
2021-04-02 11:49:00 -0700

    update

--- configs/backend.sh
+++ configs/backend.sh
@@ -83,21 +83,23 @@ sudo service consul start
 sudo service consul status
 
 #Install Dockers
-sudo snap install docker
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+#sudo snap install docker
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
 
 #Run  nginx
-sleep 10
-cat << EOF > docker-compose.yml
-version: "3.7"
-services:
-  web:
-    image: nginxdemos/hello
-    ports:
-    - "80:80"
-    restart: always
-    command: [nginx-debug, '-g', 'daemon off;']
-    network_mode: "host"
-EOF
-sudo docker-compose up -d
+#sleep 10
+#cat << EOF > docker-compose.yml
+#version: "3.7"
+#services:
+#  web:
+#    image: nginxdemos/hello
+#    ports:
+#    - "80:80"
+#    restart: always
+#    command: [nginx-debug, '-g', 'daemon off;']
+#    network_mode: "host"
+#EOF
+#sudo docker-compose up -d
+
+sudo apt update && sudo apt install -y nginx
